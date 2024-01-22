2185543eec331907836cf0cf6cc92e11db0e53cb
kizutara <loidddkun@gmail.com>
2024-01-22 21:37:13 +0900

    lisheng(edited)

--- 39_ZhengWai_Lab08/Assets/CalculatorScript.cs
+++ 39_ZhengWai_Lab08/Assets/CalculatorScript.cs
@@ -9,10 +9,18 @@ public class CalculatorScript : MonoBehaviour
     public Text DebuggingText;
     public Toggle toggleUSD;
     public Toggle toggleJPY;
+    public Toggle toggleRM;
+    public Toggle toggleEUR;
+    public Toggle toggleKRW;
+    public Toggle toggleTWD;
     public InputField Amount;
     public float amount = 0f;
     public float SGDUSD_rate = 0.75f;
     public float SGDJPY_rate = 82.78f;
+    public float SGDRM_rate = 3.08f;
+    public float SGDEUR_rate = 0.63f;
+    public float SGDKRW_rate = 881.54f;
+    public float SGDTWD_rate = 20.73f;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,18 +33,33 @@ public class CalculatorScript : MonoBehaviour
 
     public void Converted()
     {
-        float amount = float.Parse(Amount.text);
-        if (toggleUSD.isOn == true && toggleJPY.isOn == false)
+        try
         {
-            outputConvertedAmount.text = "$" + (amount * SGDUSD_rate);
+            float amount = float.Parse(Amount.text);
+            if (toggleUSD.isOn == true)
+            {
+                outputConvertedAmount.text = "$" + (amount * SGDUSD_rate);
+            }
+            else if (toggleJPY.isOn == true)
+            {
+                outputConvertedAmount.text = "¥" + (amount * SGDJPY_rate);
+            }
+            else if (toggleRM.isOn == true)
+            {
+                outputConvertedAmount.text = "RM" + (amount * SGDRM_rate);
+            }
+            else if (toggleEUR.isOn == true)
+            {
+                outputConvertedAmount.text = "¥" + (amount * SGDEUR_rate);
+            }
+            else if (toggleKRW.isOn == true)
+            {
+                outputConvertedAmount.text = "¥" + (amount * SGDKRW_rate);
+            }
         }
-        else if (toggleJPY.isOn == true && toggleUSD.isOn == false)
+        catch(System.FormatException)
         {
-            outputConvertedAmount.text = "¥" + (amount * SGDJPY_rate);
-        }
-        else if (toggleJPY.isOn == true && toggleUSD.isOn == true)
-        {
-            DebuggingText.text = "please select only one currency";
+            DebuggingText.text = "Please Enter Valid Amount";
         }
     }
 
