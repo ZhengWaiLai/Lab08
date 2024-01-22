a35f192a52d790e4c42c3635756003fe4f062257
ZhengWaiLai <laizhengwai05@gmail.com>
2024-01-22 12:28:22 +0800

    Lab08

---
+++ 39_ZhengWai_Lab08/Assets/CalculatorScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CalculatorScript : MonoBehaviour
+{
+    public InputField outputConvertedAmount;
+    public Text DebuggingText;
+    public Toggle toggleUSD;
+    public Toggle toggleJPY;
+    public InputField Amount;
+    public float amount = 0f;
+    public float SGDUSD_rate = 0.75f;
+    public float SGDJPY_rate = 82.78f;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Converted()
+    {
+        float amount = float.Parse(Amount.text);
+        if (toggleUSD.isOn == true && toggleJPY.isOn == false)
+        {
+            outputConvertedAmount.text = "$" + (amount * SGDUSD_rate);
+        }
+        else if (toggleJPY.isOn == true && toggleUSD.isOn == false)
+        {
+            outputConvertedAmount.text = "¥" + (amount * SGDJPY_rate);
+        }
+        else if (toggleJPY.isOn == true && toggleUSD.isOn == true)
+        {
+            DebuggingText.text = "please select only one currency";
+        }
+    }
+
+    public void Clear()
+    {
+        outputConvertedAmount.text = "";
+        Amount.text = "";
+        toggleUSD.isOn = false;
+        toggleJPY.isOn = false;
+    }
+}
